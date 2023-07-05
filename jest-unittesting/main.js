dad3bd311c08227b1ad8a8ecab983e1a2172e7fe
Blaise <ejikemeblaise08@gmail.com>
2023-07-05 21:25:10 +0100

    user-testcase

--- jest-unittesting/main.js
+++ jest-unittesting/main.js
@@ -1,52 +1,23 @@
-class Admin {
-    firstName;
-    lastName;
-    middleName;
-
-    constructor(data = {}) {
-    
-        this.firstName = data.firstName || "" ;
-        this.lastName = data.lastName   || ""; 
-        this.middleName = data.middleName || "";
-    }
-}
-
-//write some test by:
-//group it with "describe"
-//test 'it'
-
 // Test Suite 
-describe(`${Admin.name} Class`, () => {
+describe(`${User} Class`, () => {
+    let model;
+    
+    beforeEach(() => {
+        model = new User();
+    });
+    
     describe('default values', () => {
         it('first name defaults to empty', () => {
-        // arrange
-        const data = { firstName: null };
-        
-        // act
-        const model = new Admin(data);
-        
-        // assert
-        expect(model.firstName).toBe('');
+            // assert
+            expect(model.firstName).toBe('');
         });
     
         it('last name defaults to empty', () => {
-            // arrange
-            const data = { lastName: null };
-            
-            // act
-            const model = new Admin(data);
-            
             // assert
             expect(model.lastName).toBe('');
         });
     
         it('middle name defaults to empty', () => {
-            // arrange
-            const data = { middleName: null };
-            
-            // act
-            const model = new Admin(data);
-            
             // assert
             expect(model.middleName).toBe('');
         }); 
